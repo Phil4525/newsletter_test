419234c3ef5d9f1eacbf621ab353d983dca8af62
philippe mariou <philippe.mariou@colombbus.org>
2021-09-16 15:11:01 +0200

    Newsletter fonctionnelle

--- src/Controller/NewslettersController.php
+++ src/Controller/NewslettersController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\Newsletters\NewslettersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
@@ -78,15 +79,122 @@ class NewslettersController extends AbstractController
     }
 
     /**
-     * @Route("/prepare", name="prepare")
+     * @Route("/create", name="create")
      */
     public function prepare(Request $request): Response
     {
         $newsletter = new Newsletters();
         $form = $this->createForm(NewslettersType::class, $newsletter);
 
-        return $this->render('newsletters/prepare.html.twig', [
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($newsletter);
+            $em->flush();
+
+            return $this->redirectToRoute('newsletters_list');
+        }
+
+        return $this->render('newsletters/create.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/list", name="list")
+     */
+    public function list(NewslettersRepository $newsletters): Response
+    {
+        return $this->render('newsletters/list.html.twig', [
+            'newsletters' => $newsletters->findAll(),
+        ]);
+    }
+
+     /**
+     * @Route("/send/{id}", name="send")
+     */
+    public function send(Newsletters $newsletter, MailerInterface $mailer): Response
+    {
+        $users = $newsletter->getCategories()->getUsers();
+
+        foreach($users as $user) {
+            if($user->getIsvalid()) {
+                $email = (new TemplatedEmail())
+                    ->from('newsletter@site.fr')
+                    ->to($user->getEmail())
+                    ->subject($newsletter->getName())
+                    ->htmlTemplate('emails/newsletter.html.twig')
+                    ->context(compact('newsletter', 'user'))
+                ;
+                $mailer->send($email);
+            }
+        }
+
+        $newsletter->setIsSent(true);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($newsletter);
+        $em->flush();
+
+        return $this->redirectToRoute('newsletters_list');
+    }
+
+    /**
+     * @Route("/unsubscribe/{id}/{newsletter}/{token}", name="unsubscribe")
+     */
+    public function unsubscribe(Users $user, Newsletters $newsletter, $token): Response
+    {
+        if($user->getValidationToken() != $token) {
+            throw $this->createNotFoundException('Page non trouvée');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        if(count($user->getCategories()) > 1) {
+            $user->removeCategory($newsletter->getCategories());
+            $em->persist($user);
+        } else {
+            $em->remove($user);
+        }
+        $em->flush();
+
+        $this->addFlash('message', 'Vous êtes désabonné de la newsletter.');
+
+        return $this->redirectToRoute('app_home');
+    }
+
+    /**
+     * @Route("/{id}/edit", name="edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Newsletters $newsletter): Response
+    {
+        $form = $this->createForm(NewslettersType::class, $newsletter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('newsletters_list');
+        }
+
+        return $this->renderForm('newsletters/edit.html.twig', [
+            'newsletter' => $newsletter,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="delete", methods={"POST"})
+     */
+    public function delete(Request $request, Newsletters $newsletter): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$newsletter->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($newsletter);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('newsletters_list', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Entity/Newsletters/Users.php
+++ src/Entity/Newsletters/Users.php
@@ -6,9 +6,12 @@ use App\Repository\Newsletters\UsersRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UsersRepository::class)
+ * @UniqueEntity("email")
  */
 class Users
 {
@@ -20,7 +23,8 @@ class Users
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(name="email", type="string", length=255, nullable=true, unique=true)
+     * @Assert\Email
      */
     private $email;
 
